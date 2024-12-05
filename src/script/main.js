6ce031e6fa82a84dfa24bddb0664bd72be07a46d
bfaatime <fatimebexseliyeva@icloud.com>
2024-12-05 17:34:33 +0400

    Zeyneb

--- src/script/main.js
+++ src/script/main.js
@@ -1,3 +1,16 @@
-import { nanoid } from "nanoid";
-
-console.log(nanoid());
+const user = document.querySelector(".user")
+const overlay = document.querySelector(".overlay")
+const ul = document.querySelector(".overlay ul")
+overlay.style.height = "0" 
+ul.style.opacity = "0"
+user.addEventListener("click", function(){
+    if(overlay.style.height == "100px" ){
+        overlay.style.height = "0" 
+        ul.style.opacity = "0"
+    }
+    else{
+        overlay.style.height = "100px" 
+        ul.style.opacity = "1"
+    }
+    
+})
